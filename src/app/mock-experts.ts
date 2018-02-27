b44e1e814e30a8114c97803bd04ec2542e2e350f
Fillosof <yurij.krot@gmail.com>
2018-02-27 11:14:56 +0200

    added search delay and some pixel makeup

--- src/app/mock-experts.ts
+++ src/app/mock-experts.ts
@@ -1,14 +1,14 @@
 import { Expert } from './expert';
 
 export const EXPERTS: Expert[] = [
-  { numbers: 11, tech: 'Angular' },
-  { numbers: 12, tech: 'Java' },
-  { numbers: 13, tech: 'JavaScript' },
+  { numbers: 21, tech: 'Angular' },
+  { numbers: 4, tech: 'Java' },
+  { numbers: 3, tech: 'JavaScript' },
   { numbers: 14, tech: '.NET' },
   { numbers: 15, tech: 'Java2EE' },
   { numbers: 16, tech: 'JasperReports' },
   { numbers: 17, tech: 'Java SE 8' },
   { numbers: 18, tech: 'Java Script ES5' },
-  { numbers: 19, tech: 'Angular 5+' },
+  { numbers: 69, tech: 'Angular 5+' },
   { numbers: 20, tech: 'AngularJS' }
 ];
--- src/app/search-by-expert/search-by-expert.component.ts
+++ src/app/search-by-expert/search-by-expert.component.ts
@@ -11,7 +11,10 @@ export class SearchByExpertComponent implements OnInit {
   experts = EXPERTS;
   searchExpert = '';
   showModal = false;
+  showAfterSearching = false;
   findedExperts: Expert;
+  searchDelayTimer = null;
+
 
   constructor() { }
 
@@ -20,6 +23,26 @@ export class SearchByExpertComponent implements OnInit {
 
   toogleModal () {
     this.showModal = !this.showModal;
+  }
+
+
+  showSearcingDelay(event) {
+    const searchInput = (<HTMLInputElement>event.target).value;
+
+    if (searchInput) {
+      this.showAfterSearching = true;
+      clearTimeout(this.searchDelayTimer);
+      this.searchDelayTimer = setTimeout(() => {
+        this.showAfterSearching = false;
+        this.searchExpert = searchInput;
+      }, 500);
+    } else {
+      clearTimeout(this.searchDelayTimer);
+      this.showAfterSearching = false;
+      this.searchExpert = '';
+    }
+
+
   }
 
   modal (expert) {
